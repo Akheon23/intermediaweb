a1c92b69a571a288a77f20b896f92dee3c162bff
Christopher Werkshage <noreply>
2016-12-03 09:09:07 +0000

    Add component intermedia

---
+++ ui/intermedia.reel/intermedia.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/intermedia.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Intermedia
+ * @extends Component
+ */
+exports.Intermedia = Component.specialize(/** @lends Intermedia# */ {
+    constructor: {
+        value: function Intermedia() {
+            this.super();
+        }
+    }
+});
